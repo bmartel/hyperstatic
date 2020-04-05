8a6095429049e613ad0d1c00325032503989510b
Brandon Martel <brandonmartel@gmail.com>
2020-04-05 09:59:45 -0500

    try catch on the router to allow notfound to render in cases of error

--- src/Router.js
+++ src/Router.js
@@ -3,39 +3,46 @@ import { h } from "hyperapp";
 import { htmlToVdom } from "./htmlToVdom";
 
 // Router component
-export function Router(state, fallback = "Loading ...", notFound = "404") {
-  const matchedRoute = state.routes[state.location.route];
+export function Router(
+  state,
+  fallback = "Loading ...",
+  notFound = "Page Not Found"
+) {
+  try {
+    const matchedRoute = state.routes[state.location.route];
 
-  let { component, guard } = matchedRoute;
+    let { component, guard } = matchedRoute;
 
-  if (!component) {
-    component = matchedRoute;
-  }
+    if (!component) {
+      component = matchedRoute;
+    }
 
-  // Render a notFound component when route is unmatched or failed guard condition
-  if (!component || (guard && !guard(state))) {
-    return notFound;
-  }
+    // Render a notFound component when route is unmatched or failed guard condition
+    if (!component || (guard && !guard(state))) {
+      return notFound;
+    }
 
-  const pageData = state.pageData[state.location.path];
+    const pageData = state.pageData[state.location.path];
 
-  if (component.view) {
-    if (!component.initAction) {
-      return h("div", { id: "router-outlet" }, [component.view(state)]);
-    } else {
-      if (pageData && pageData.initiated) {
+    if (component.view) {
+      if (!component.initAction) {
         return h("div", { id: "router-outlet" }, [component.view(state)]);
+      } else {
+        if (pageData && pageData.initiated) {
+          return h("div", { id: "router-outlet" }, [component.view(state)]);
+        }
       }
     }
-  }
 
-  const previousOutlet = document.getElementById("router-outlet");
-  if (previousOutlet) {
-    return h("div", { id: "router-outlet" }, [
-      htmlToVdom(previousOutlet.innerHTML)
-    ]);
+    const previousOutlet = document.getElementById("router-outlet");
+    if (previousOutlet) {
+      return h("div", { id: "router-outlet" }, [
+        htmlToVdom(previousOutlet.innerHTML),
+      ]);
+    }
+    // Render a loading or intermediary fallback
+    return fallback;
+  } catch (err) {
+    return notFound;
   }
-
-  // Render a loading or intermediary fallback
-  return fallback;
 }
