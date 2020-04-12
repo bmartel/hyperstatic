85877cc8f4e8cb9d5af9dee777772b7cb5b6df40
Brandon Martel <brandon@pepper.com>
2020-04-12 17:42:23 -0500

    fixing Lifecycle dispatch

--- src/Lifecycle.js
+++ src/Lifecycle.js
@@ -11,7 +11,7 @@ export const Lifecycle = (elementName, props, children) => {
   const fn = (method, eventName) =>
     function (el) {
       const event = new CustomEvent(eventName, { detail: el });
-      setTimeout(() => el.parentElement.dispatchEvent(event));
+      setTimeout(() => el.dispatchEvent(event));
       return Object.getPrototypeOf(this)[method].call(this, el);
     };
   return h(
